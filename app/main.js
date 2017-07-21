16fd56389ee4596eb45ff0c22b8004fb00a70f49
Alberto Gimenez <albergimenez@gmail.com>
2017-07-21 08:35:18 -0400

    Features de Google Analytics

--- app/main.js
+++ app/main.js
@@ -8,18 +8,24 @@ require([
     "dojo/json",
     "app/views/LoadingView",
     "app/Application",
+    "app/lib/GoogleAnalytics",
     "dojo/text!./app/config.json",
     "bootstrap/Collapse",
     "bootstrap/Tab",
     "bootstrap/Popover",
     "bootstrap/Tooltip",
     "bootstrap/Typeahead",
-    "dojo/domReady!"], function(declare, JSON, LoadingView, Application, config) {
+    "dojo/domReady!"], function(declare, JSON, LoadingView, Application,
+                                GoogleAnalytics, config) {
 
     window.CONFIG = JSON.parse(config);
 
     var asu = new Application();
     asu.startup();
+
+    var ga = new GoogleAnalytics();
+    ga.startup();
+
     var loading = new LoadingView("loadingOverlay");
     loading.endLoading();
 });
